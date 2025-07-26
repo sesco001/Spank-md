25fd6829ce3d55edec117f7b7ac3d76eae80d673
sesco001 <marcasmatheka@gmail.com>
2025-07-27 02:23:11 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "JINX-XMD",
   botOwner: 'Professor', // Your name
-  ownerNumber: '254112192119', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254769995625', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
