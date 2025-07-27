67fda68f652bd44987262bac180814426e4bde89
sesco001 <marcasmatheka@gmail.com>
2025-07-27 11:19:12 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: 'JINX-XMD',
+  packname: 'SPANK-XMD',
   author: '‎',
-  botName: "JINX-XMD",
-  botOwner: 'Professor', // Your name
+  botName: "SPANK-XMD",
+  botOwner: 'MAKAMESCO', // Your name
   ownerNumber: '254769995625', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
